ad986e571f8740af7f457e6d3745ff13e3d1e17c
Jason <112602054+litaomiss@users.noreply.github.com>
2026-04-17 03:34:46 +0000

    404错误处理

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import Home from '../pages/Home.vue'
 import Every5 from '../pages/Every5Minutes.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     { path: '/', component: Home },
     { path: '/every-5-minutes', component: Every5 }
